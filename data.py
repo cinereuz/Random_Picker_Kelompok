d3e3f1d36543c879362681174b2d571cb0b3939f
cinereuz <fauzananshari88@gmail.com>
2023-09-01 07:52:45 +0800

    update data siswa

--- data.py
+++ data.py
@@ -1,16 +1,46 @@
 class_data = {
-    'Kelas_XII_Social': ['Siswa1', 'Siswa2', 'Siswa3', 'Siswa4', 'Siswa5'],
-    'Kelas_XII_1': {
+    'XII IPS': {
         'mixed' : [
-            'Adithya Maulana Ardiyanto', 'Ahmad Fikri Hussein', 'Anugerah Fajar Ridho', 'Danang Ramadhan Al Farisi', 'Dika Fahreza Yusna', 'Dimas Triaputra Nitomo', 'M. Reza Rahman', 'Muhammad Fauzan Anshari', 'Muhammad Nur Ihsan', 'Muhammad Yafi Nur Rasyid','Ainun Nafisah', 'Alisa Rohima', 'Alya Neshela', 'Arsy Najla Izzami Rudin', 'Ayudya Nursalsabila El Dzikri', 'Dina Hilyati', 'Helma Syarifah', 'Lutfia Bilqis', 'Nuzula Aliya Rahima', 'Siti Cissa Nur Azzahra'
+            'Ahmad Sayuti', 'Muhammad Alif Ichsan', 'Muhammad Gazali Annur', 'Alya Zidan Riyadi', 'Arief Muhammad Ilham', 'Muhammad Rifaldi Najwan', 'Daffa Naasyarudin', 'Muhammad Rizal Firdaus', 'M. Fazrul Amin', 'Muhammad Afif Rafiqy', 'Thoriqul Ihsan Kurniawan', 'Alisya Pratiwi', 'Aminatu Jahra', 'Fakhrina Istiqomah', 'Farrah Nayla Nurdian', 'Inayah Ramadhani', 'Luthfia Khumaira', 'Naura Hidayati', 'Nazwa Puteri', 'Nor Anida Hanifa'
+                   ],
+        'male' : [
+            'Ahmad Sayuti', 'Muhammad Alif Ichsan', 'Muhammad Gazali Annur', 'Alya Zidan Riyadi', 'Arief Muhammad Ilham', 'Muhammad Rifaldi Najwan', 'Daffa Naasyarudin', 'Muhammad Rizal Firdaus', 'M. Fazrul Amin', 'Muhammad Afif Rafiqy', 'Thoriqul Ihsan Kurniawan'
+        ],
+        'female' : [
+            'Alisya Pratiwi', 'Aminatu Jahra', 'Fakhrina Istiqomah', 'Farrah Nayla Nurdian', 'Inayah Ramadhani', 'Luthfia Khumaira', 'Naura Hidayati', 'Nazwa Puteri', 'Nor Anida Hanifa'
+        ]
+    },
+    'XII MIPA 1': {
+        'mixed' : [
+            'Adithya Maulana Ardiyanto', 'Ahmad Fikri Hussein', 'Anugerah Fajar Ridho', 'Danang Ramadhan Al Farisi', 'Dika Fahreza Yusna', 'Dimas Triaputra Nitomo', 'M. Reza Rahman', 'Muhammad Fauzan Anshari', 'Muhammad Nur Ihsan', "Muhammad Yafi' Nur Rasyid",'Ainun Nafisah', 'Alisa Rohima', 'Alya Neshela', 'Arsy Najla Izzami Rudin', 'Ayudya Nursalsabila El Dzikri', 'Dina Hilyati', 'Helma Syarifah', 'Lutfia Bilqis', 'Nuzula Aliya Rahima', 'Siti Cissa Nur Azzahra'
         ], 
         'male' : [
-            'Adithya Maulana Ardiyanto', 'Ahmad Fikri Hussein', 'Anugerah Fajar Ridho', 'Danang Ramadhan Al Farisi', 'Dika Fahreza Yusna', 'Dimas Triaputra Nitomo', 'M. Reza Rahman', 'Muhammad Fauzan Anshari', 'Muhammad Nur Ihsan', 'Muhammad Yafi Nur Rasyid'
+            'Adithya Maulana Ardiyanto', 'Ahmad Fikri Hussein', 'Anugerah Fajar Ridho', 'Danang Ramadhan Al Farisi', 'Dika Fahreza Yusna', 'Dimas Triaputra Nitomo', 'M. Reza Rahman', 'Muhammad Fauzan Anshari', 'Muhammad Nur Ihsan', "Muhammad Yafi' Nur Rasyid"
             ],
         'female' : [
             'Ainun Nafisah', 'Alisa Rohima', 'Alya Neshela', 'Arsy Najla Izzami Rudin', 'Ayudya Nursalsabila El Dzikri', 'Dina Hilyati', 'Helma Syarifah', 'Lutfia Bilqis', 'Nuzula Aliya Rahima', 'Siti Cissa Nur Azzahra'
             ]        
     },
-    'Kelas_XII_2': ['Siswa11', 'Siswa12', 'Siswa13', 'Siswa14', 'Siswa15'],
-    'Kelas_XII_3': ['Siswa11', 'Siswa12', 'Siswa13', 'Siswa14', 'Siswa15']
+    'XII MIPA 2': {
+        'mixed' : [
+            'Muhammad Farras Faqih Fiddin', 'Muhammad Fawwaaz Ramadhan', 'Ahmad Fitria Ramadhani', 'Muhammad Rafly Shiddieq', 'Muhammad Rofiq akbar', 'Faris Rizqian Najmi', 'Musthafa Hadi Ulya', 'Nandika Maynanza', 'Maulana Husein', 'Hasna Nisrina Aisyah', 'Alisya Bunga Tiara Wahdah', 'Aulia Rahmatunnisa', 'Azka Nafisah', 'Doria Safiq', 'Intan Mahardhika', 'Mazaya Rizka Alya', 'Syamila Ghina Tiara Putri', 'Wafiq Alya Fatimah', 'Geara Elvira Amelia'
+        ],
+        'male' : [
+            'Muhammad Farras Faqih Fiddin', 'Muhammad Fawwaaz Ramadhan', 'Ahmad Fitria Ramadhani', 'Muhammad Rafly Shiddieq', 'Muhammad Rofiq akbar', 'Faris Rizqian Najmi', 'Musthafa Hadi Ulya', 'Nandika Maynanza', 'Maulana Husein'
+        ],
+        'female' : [
+            'Hasna Nisrina Aisyah', 'Alisya Bunga Tiara Wahdah', 'Aulia Rahmatunnisa', 'Azka Nafisah', 'Doria Safiq', 'Intan Mahardhika', 'Mazaya Rizka Alya', 'Syamila Ghina Tiara Putri', 'Wafiq Alya Fatimah', 'Geara Elvira Amelia'
+        ]
+    },
+    'XII MIPA 3': {
+        'mixed' : [
+            "Achmad Sa'id Al Bukhari", 'Muhammad Fathir Saurie', 'Muhammad Irfan Kamil', 'Bayu Kurniawan', 'Muhammad Wildan Anshari', 'Irsyad Nur Rahman', 'Muhammad Zairin Maulana Al Wahhaj', 'Nuhdias Rizki', 'Muhammad Akmal Salim Taqiyuddin', 'Aliya Sandrina', 'Anisa Rahmah', 'Aulia Rizqi Ramadhani', 'Dea Fitri Amalia', "Habibah Sai'dah", 'Intan Rahmanafartiwi', 'Nabila Nur Afifah', 'Najwa Maulida', 'Qotratun Nada Salsabila', 'Thania Yasa Hendryana'
+        ],
+        'male' : [
+            "Achmad Sa'id Al Bukhari", 'Muhammad Fathir Saurie', 'Muhammad Irfan Kamil', 'Bayu Kurniawan', 'Muhammad Wildan Anshari', 'Irsyad Nur Rahman', 'Muhammad Zairin Maulana Al Wahhaj', 'Nuhdias Rizki', 'Muhammad Akmal Salim Taqiyuddin'
+        ],
+        'female' : [
+            'Aliya Sandrina', 'Anisa Rahmah', 'Aulia Rizqi Ramadhani', 'Dea Fitri Amalia', "Habibah Sai'dah", 'Intan Rahmanafartiwi', 'Nabila Nur Afifah', 'Najwa Maulida', 'Qotratun Nada Salsabila', 'Thania Yasa Hendryana'
+        ]
+    }
 }
